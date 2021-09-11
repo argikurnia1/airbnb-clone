99e5261931ec0b1739f8e1786cc9a5e759ba4d14
argikurnia <argikurnia2@gmail.com>
2021-09-11 15:24:17 +0700

    initial banner

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Banner from "../components/Banner";
 
-import Header from '../components/Header'
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -10,8 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header/>
-
+      <Header />
+      <Banner />
     </div>
-  )
+  );
 }
