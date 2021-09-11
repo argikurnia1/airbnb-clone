fbf4a0a136712b27ec84549e4a529cff20914c5f
argikurnia <argikurnia2@gmail.com>
2021-09-11 13:24:10 +0700

    initial header

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+function Header() {
+    return (
+        <header>
+            <h1>I am the header</h1>
+             
+            {/* left */}
+            <div>
+                
+            </div>
+
+            {/* middle */}
+             <div></div>
+            {/* right */}
+            <div></div>
+        </header>
+    )
+}
+
+export default Header
