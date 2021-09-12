819b4bb0a02f30fccc2a5e38fba68cf399a40cb6
argikurnia <argikurnia2@gmail.com>
2021-09-12 21:34:26 +0700

    final

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
-  env: {
-    mapbox_key:
-      "pk.eyJ1IjoiYXJnaWt1cm5pYSIsImEiOiJja3RoMzgzamowbnR4MzBuNDNmaG85NjJsIn0.Hka7FOsdw3YLpUlPhUl4IA",
-  },
 };
