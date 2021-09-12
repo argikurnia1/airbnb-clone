38f358236e501b12c29befb1bd6fc79d00bc9436
argikurnia <argikurnia2@gmail.com>
2021-09-12 16:35:44 +0700

    Info card Search Progressbar

--- components/Header.js
+++ components/Header.js
@@ -7,16 +7,18 @@ import {
   UsersIcon,
 } from "@heroicons/react/solid";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
 
-function Header() {
+function Header(props) {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [noOfGuests, setNoGuests] = useState(1);
+  const router = useRouter();
 
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
@@ -25,6 +27,18 @@ function Header() {
 
   const resetInput = () => {
     setSearchInput("");
+  };
+
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests: noOfGuests,
+      },
+    });
   };
 
   const selectionRange = {
@@ -36,7 +50,10 @@ function Header() {
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
       {/* left */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -54,7 +71,7 @@ function Header() {
           }}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           type="text"
-          placeholder="Start your search"
+          placeholder={props.placeholder || "Start your search"}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
@@ -94,7 +111,9 @@ function Header() {
             <button className="flex-grow text-gray-500" onClick={resetInput}>
               Cancel
             </button>
-            <button className="flex-grow text-red-500">Search</button>
+            <button onClick={search} className="flex-grow text-red-500">
+              Search
+            </button>
           </div>
         </div>
       )}
---
+++ components/InfoCard.js
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { HeartIcon } from "@heroicons/react/outline";
+import { StarIcon } from "@heroicons/react/solid";
+
+function InfoCard(props) {
+  return (
+    <div className="flex py-7 px-2 pr-4 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t">
+      <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
+        <Image
+          src={props.img}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-2xl"
+        />
+      </div>
+
+      <div className="flex flex-col flex-grow pl-5">
+        <div className="flex justify-between">
+          <p>{props.location}</p>
+          <HeartIcon className="h-7 cursor-pointer" />
+        </div>
+        <h4 className="text-xl">{props.title}</h4>
+
+        <div className="border-b w-10 pt-2" />
+
+        <p className="pt-2 text-sm text-gray-500 flex-grow">
+          {props.description}
+        </p>
+
+        <div className="flex justify-between items-end pt-5">
+          <p className="flex items-center">
+            <StarIcon className="h-5 text-red-400" />
+            {props.star}
+          </p>
+
+          <div>
+            <p className="text-lg lg:text-2xl font-semibold pb-2">
+              {props.total}
+            </p>
+            <p className="text-right font-extralight">{props.price}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InfoCard;
